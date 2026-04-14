9be0cb14acd72269ef106abd6807900dabd341e9
freedo56 <dtordoc@gmail.com>
2026-04-14 16:31:45 +0700

    Phase 1.5 : Make memory for AI

--- main.py
+++ main.py
@@ -6,11 +6,26 @@ load_dotenv()
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-response = client.chat.completions.create(
-    model="llama-3.3-70b-versatile",
-    messages=[
-        {"role": "user", "content": "สวัสดี บอกชื่อตัวเองหน่อย"}
-    ]
-)
+messages = []
 
-print(response.choices[0].message.content)
+print("คุยกับ AI ได้เลย! พิมพ์ 'exit' เพื่อออก")
+
+while True:
+    user_input = input("คุณ: ")
+    
+    if user_input == "exit":
+        print("ลาก่อนครับ!")
+        break
+    
+    messages.append({"role": "user", "content": user_input})
+    
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=messages
+    )
+    
+    ai_reply = response.choices[0].message.content
+    
+    messages.append({"role": "assistant", "content": ai_reply})
+    
+    print(f"AI: {ai_reply}")
